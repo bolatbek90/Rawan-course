8c3d97cb104b1d1a214064e2f9ef3ebf00528efd
unknown <zhangaliev.b@gmail.com>
2021-06-19 03:11:54 +0500

    '19/06/21'

--- main.py
+++ main.py
@@ -1,45 +1,63 @@
 import random
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get('/')
-def home():
-    return 'Salemberdik'
+def home(request: Request):
+    return templates.TemplateResponse('index.html', {
+        'request': request,
+        'name': 'Оқырман'
+    })
 
 
-@app.get('/bloopers')
-def bloopers():
-    bloopers = [
-        'Bir bala jaqsy, al basqa bala okinishke orai',
-        'Bala jasaganda bizden ryqsat syragan joqsyndar goi',
-        'Eki siyr bagyp oz ozindi asyra',
-        'Nakonecto u nas koronovirus',
-        'Potomu shto potomu shto',
-        'Salut dlya podderjki vrachei',
+@app.get('/advice')
+def advice(request: Request):
+    advice = [
+        'Өмір бақи джун бол',
+        'Тек пайда әкелуге тырыс',
+        'Күнделікті жұмысыңды саналы тәжірибеге айналдыр',
+        'Басшыңмен байланыста бол',
+        'Таза код кітабын оқы',
+        'Спандж мультфилмі презентацияға жақсы',
     ]
-    result = random.choice(bloopers)
-    return result
+    advice = random.choice(advice)
+    return templates.TemplateResponse('advice.html', {
+        'request': request,
+        'advice': advice
+    })
 
 
 # colleagues
 
-colleagues = ['mariam', 'ali']
+colleagues = ['нұржан', 'ұлжан', 'анар', 'айбану']
 
 colleagues_db = {
-    'mariam': {
-        'department': 'managment',
-        'age': 21
+    'нұржан': {
+        'қаласы': 'көкшетау',
+        'жасы': 28,
+        'жұмысы': 'астрофизика(жұлдыздар емес)'
     },
-    'ali': {
-        'department': 'IT',
-        'age': 25
+    'ұлжан': {
+        'қаласы': 'тараз',
+        'жасы': 28,
+        'жұмысы': 'project manager'
     },
-    'sergey': {
-        'department': 'IT',
-        'age': 26
+    'анар': {
+        'қаласы': 'семей',
+        'жасы': 35,
+        'жұмысы': 'стартап'
+    },
+    'айбану': {
+        'қаласы': 'шымкент',
+        'жасы': 23,
+        'жұмысы': 'PHP & Python Developer'
     }
 }
 
@@ -61,7 +79,6 @@ class RequestAPI:
         return quote['content']
 
     def get_text_with_quote_for_name(self, name):
-
         result = 'Досым %s. Мынау менің саған кеңесім: %s' % (name.capitalize(), self.get_content())
         return result
 
@@ -69,16 +86,23 @@ class RequestAPI:
 # colleagues route
 
 @app.get('/colleagues')
-def colleagues():
-    return colleagues_db
+def colleagues(request: Request):
+    return templates.TemplateResponse('colleagues.html', {
+        'request': request,
+        'colleagues': colleagues_db
+    })
 
 
 # personel colleagues route
 
 @app.get('/colleagues/{name}')
-def colleagues(name):
+def colleagues(request: Request, name):
     if name in colleagues_db:
-        return colleagues_db[name]
+        return templates.TemplateResponse('colleagues.html', {
+            'request': request,
+            'name': name,
+            'info': colleagues_db[name]
+        })
     else:
         return 'Qate'
 
@@ -89,3 +113,21 @@ def colleagues(name):
 
     return my_request.get_text_with_quote_for_name(name)
 
+@app.get('/weather')
+def weather(request: Request):
+    return templates.TemplateResponse('weathermain.html', {
+        'request': request,
+    })
+
+@app.get('/weather/{city}')
+def wea(request: Request, city):
+    response = requests.get('https://goweather.herokuapp.com/weather/' + city)
+
+    if response.status_code == 200:
+        city = response.json()
+        return templates.TemplateResponse('weather.html', {
+            'request': request,
+            'city': city,
+        })
+    else:
+        return 'Wrong'
